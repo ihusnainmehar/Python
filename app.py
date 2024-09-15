e9c5f068d991f6acea7b1085ab04b6c5b1cae8f6
Husnain Mehar <shahzadahusnain.sh@gmail.com>
2024-09-15 16:52:50 +0500

    HousePriceProblem

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ price= 1000000
 has_good_credit = True
 if has_good_credit:
     downpayment = 0.1*price
-    print(f"Down Payment is 10% which is: {downpayment}")
+    print(f"Down Payment is 10% which is: ${downpayment}")
 else:
     downpayment = 0.2*price
-    print(f"Down Payment is 20% which is: {downpayment}")
+    print(f"Down Payment is 20% which is: ${downpayment}")
