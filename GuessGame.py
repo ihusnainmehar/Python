7a6f1bb520be5d70ab798fdfb378ada7482f9c0d
Husnain Mehar <shahzadahusnain.sh@gmail.com>
2024-10-16 22:22:37 +0500

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,11 +1,15 @@
-secret_number = 9
-Guess_Count = 0
-guess_limit = 3
-while Guess_Count<guess_limit:
-    Guess = int(input('Guess: '))
-    Guess_Count +=1
-    if Guess == secret_number:
+secret_number = 6
+guess_limit = 5
+guess_count = 0
+while guess_count<guess_limit:
+    guess = int(input("Enter Your Guess: "))
+    guess_count +=1
+    if guess == secret_number:
         print("You Won!")
         break
+    elif guess>secret_number:
+        print("Too High")
+    elif guess<secret_number:
+        print("Too Low")
 else:
-    print('Sorry! You Failed')
+    print("Sorry! You Failed")
